f8e015c0103ed5c5e138d2400b0795741cd0d2bc
Cimorexave <sadeq.s502@gmail.com>
2022-10-04 16:51:39 +0330

    feat: setting up login route

---
+++ routes/login.ts
@@ -0,0 +1,27 @@
+//modules
+//express
+import * as express from "express";
+const router = express.Router();
+
+router.post("/login", (req: express.Request, res: express.Response) => {
+    //check to see if there are login info in the body
+    if (!req.body) {
+        res.json({
+            success: false,
+            msg: "wrong informantion"
+        });
+        //saving login failed login attempt
+    }
+    //destructing username and password from the body
+    const {username , password} = req.body;
+    //checking to see if the username and password exist in the database
+
+    //checking to see if they are valid
+
+    //returning successfull response
+
+    //saving login history
+
+})
+
+export default router;
--- server.ts
+++ server.ts
@@ -1,13 +1,23 @@
 //modules
 //express
 import * as express from "express";
-const app = express()();
+const app = express();
 //cors
 import cors from "cors";
+
+
 //middleware
 app.use(cors());
 //parsing json req
 app.use(express.json());
+
+//routes
+import login from "./routes/login";
+app.use(login);
+
+
+
+
 //Running server
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port: ${process.env.SERVER_PORT}`)
