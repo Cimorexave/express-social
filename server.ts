7068f37e37f2348ed2691bf7fae8d56b614db2b5
Cimorexave <sadeq.s502@gmail.com>
2022-10-04 16:24:04 +0330

    feat: parsing json and using cors as middleware in the entire routes

--- server.ts
+++ server.ts
@@ -1,7 +1,14 @@
+//modules
+//express
 import * as express from "express";
 const app = express()();
-
+//cors
+import cors from "cors";
+//middleware
+app.use(cors());
+//parsing json req
+app.use(express.json());
 //Running server
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port: ${process.env.SERVER_PORT}`)
-})
+});
